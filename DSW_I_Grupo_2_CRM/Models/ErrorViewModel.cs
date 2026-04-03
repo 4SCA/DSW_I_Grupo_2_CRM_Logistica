4f90cc262a2287bf42629502ee41d9c2a7a42586
4SCA <adrianoca2004@gmail.com>
2026-04-02 19:20:55 -0500

    Commit Inicial-Creación del Proyecto

---
+++ DSW_I_Grupo_2_CRM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DSW_I_Grupo_2_CRM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
